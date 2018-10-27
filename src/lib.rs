168c3367383142b635852238826387f7d812b5a3
Alex Beregszaszi <alex@rtfs.hu>
2018-10-27 22:39:02 +0200

    Support libeci for validation

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,24 @@
 extern crate ewasm_api;
 extern crate parity_wasm;
 extern crate pwasm_utils;
+extern crate eci;
+
+fn validate_contract(code: &[u8]) -> bool {
+    let mut checker = eci::checker::EcicChecker::default(&code.to_vec());
+    checker.fire();
+    return match checker.status() {
+        eci::checklist::CheckStatus::Unknown => true,
+        eci::checklist::CheckStatus::Nonexistent => false,
+        eci::checklist::CheckStatus::Malformed => false,
+        eci::checklist::CheckStatus::Good => true,
+    }
+}
 
 fn inject_metering(code: &[u8]) -> Result<Vec<u8>, parity_wasm::elements::Error> {
+    if !validate_contract(code) {
+        return Err(parity_wasm::elements::Error::Other("Contract doesn't meet ECI/EEI restrictions."));
+    }
+
     let module = parity_wasm::deserialize_buffer(&code)?;
 
     let result = match pwasm_utils::inject_gas_counter(module, &Default::default()) {
