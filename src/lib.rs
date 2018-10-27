ce95ef29b28a8c299c80c5c120405451f79fd358
Alex Beregszaszi <alex@rtfs.hu>
2018-10-27 22:09:24 +0200

    Properly handle errors

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,17 @@ extern crate ewasm_api;
 extern crate parity_wasm;
 extern crate pwasm_utils;
 
+fn inject_metering(code: &[u8]) -> Result<Vec<u8>, parity_wasm::elements::Error> {
+    let module = parity_wasm::deserialize_buffer(&code)?;
+
+    let result = match pwasm_utils::inject_gas_counter(module, &Default::default()) {
+        Ok(output) => output,
+        Err(_) => return Err(parity_wasm::elements::Error::Other("Metering injection failed.")),
+    };
+
+    parity_wasm::serialize(result)
+}
+
 #[no_mangle]
 pub extern "C" fn main() {
     let code = ewasm_api::calldata_acquire();
@@ -9,12 +20,9 @@ pub extern "C" fn main() {
     let total_cost = 32 * code.len();
     ewasm_api::consume_gas(total_cost as u64);
 
-    let module = parity_wasm::deserialize_buffer(&code).expect("Failed to load module");
-
-    let result = pwasm_utils::inject_gas_counter(module, &Default::default())
-        .expect("Failed to inject gas. Some forbidden opcodes?");
-
-    let output = parity_wasm::serialize(result).expect("Failed to serialize");
-
-    ewasm_api::finish_data(&output);
+    match inject_metering(&code) {
+        Ok(output) => ewasm_api::finish_data(&output),
+        // TODO: return reason in revert
+        Err(_) => ewasm_api::revert(),
+    }
 }
